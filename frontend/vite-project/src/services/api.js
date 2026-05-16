b069a165b2742131778281b75f84759f434dcbcf
erycpc <ericmbithiofficial@gmail.com>
2026-05-16 20:18:07 +0300

    fix: correct API baseURL to include /api prefix

--- frontend/vite-project/src/services/api.js
+++ frontend/vite-project/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://budgetbuddy-5apy.onrender.com/'
+  baseURL: 'https://budgetbuddy-5apy.onrender.com/api'
 })
 
 api.interceptors.request.use((config) => {
